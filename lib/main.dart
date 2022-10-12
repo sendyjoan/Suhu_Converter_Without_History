420485390acc616dc8cecd4f493b4d52ce136332
Sendy Joan K <69149460+sendyjoan@users.noreply.github.com>
2022-10-12 12:06:21 +0700

    Code Calculator

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,30 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final _controllerC = TextEditingController();
+  double _inputCelcius = 0;
+  double _kelvin = 0;
+  double _reamur = 0;
+
+  konvertersuhu() {
+    setState(() {
+      if (_controllerC.text.isNotEmpty) {
+        _inputCelcius = double.parse(_controllerC.text);
+        _kelvin = _inputCelcius + 273.0;
+        _reamur = _inputCelcius * 4 / 5;
+      } else {
+        _kelvin = 0;
+        _reamur = 0;
+      }
+    });
+  }
+
   @override
+  void dispose() {
+    _controllerC.dispose();
+    super.dispose();
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -40,51 +63,15 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            TextFormField(
-              keyboardType: TextInputType.number,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Column(
-                  children: [
-                    Text(
-                      "Suhu Dalam Kelvin",
-                      style: TextStyle(
-                        fontSize: 15,
-                      ),
-                    ),
-                    Text(
-                      "120",
-                      style: TextStyle(
-                        fontSize: 30,
-                      ),
-                    ),
-                  ],
-                ),
-                Column(
-                  children: [
-                    Text(
-                      "Suhu Dalam Reamur",
-                      style: TextStyle(
-                        fontSize: 15,
-                      ),
-                    ),
-                    Text(
-                      "120",
-                      style: TextStyle(
-                        fontSize: 30,
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
+            inputWidget(controllerC: _controllerC),
+            resultWidget(kelvin: _kelvin, reamur: _reamur),
             Row(
               children: [
                 Expanded(
                   child: ElevatedButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      konvertersuhu();
+                    },
                     child: Text("Konversi Suhu"),
                   ),
                 ),
@@ -93,6 +80,78 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
+    );
+  }
+}
+
+class resultWidget extends StatelessWidget {
+  const resultWidget({
+    Key? key,
+    required double kelvin,
+    required double reamur,
+  })  : _kelvin = kelvin,
+        _reamur = reamur,
+        super(key: key);
+
+  final double _kelvin;
+  final double _reamur;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        Column(
+          children: [
+            Text(
+              "Suhu Dalam Kelvin",
+              style: TextStyle(
+                fontSize: 15,
+              ),
+            ),
+            Text(
+              "$_kelvin",
+              style: TextStyle(
+                fontSize: 30,
+              ),
+            ),
+          ],
+        ),
+        Column(
+          children: [
+            Text(
+              "Suhu Dalam Reamur",
+              style: TextStyle(
+                fontSize: 15,
+              ),
+            ),
+            Text(
+              "$_reamur",
+              style: TextStyle(
+                fontSize: 30,
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class inputWidget extends StatelessWidget {
+  const inputWidget({
+    Key? key,
+    required TextEditingController controllerC,
+  })  : _controllerC = controllerC,
+        super(key: key);
+
+  final TextEditingController _controllerC;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: _controllerC,
+      keyboardType: TextInputType.number,
     );
   }
 }
